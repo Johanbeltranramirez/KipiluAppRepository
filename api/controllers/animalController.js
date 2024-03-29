4032d5be550440015de988a642b05159f0d1eaf9
Manuelyjc <114593318+Manuelyjc@users.noreply.github.com>
2024-03-28 19:09:58 -0500

    Agregar archivos no rastreados en la carpeta api

--- api/controllers/animalController.js
+++ api/controllers/animalController.js
@@ -1,4 +1,5 @@
 const Animal = require('../models/animal');
+const AnimalGet = require('../models/animalget'); // Renombramos el segundo módulo como AnimalGet
 
 module.exports = {
   createAnimal(req, res) {
@@ -19,32 +20,8 @@ module.exports = {
     });
   },
 
-  getAnimalById(req, res) {
-    const id = req.params.id; // ID del animal a buscar
-    Animal.findById(id, (err, animal) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          message: 'Error al obtener el animal',
-          error: err
-        });
-      }
-      if (!animal) {
-        return res.status(404).json({
-          success: false,
-          message: 'Animal no encontrado'
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        message: 'Animal encontrado',
-        data: animal
-      });
-    });
-  },
-
   getAllAnimals(req, res) {
-    Animal.getAll((err, animals) => {
+    AnimalGet.getAll((err, animals) => { // Utilizamos AnimalGet en lugar de Animal
       if (err) {
         return res.status(500).json({
           success: false,
@@ -55,44 +32,7 @@ module.exports = {
       return res.status(200).json({
         success: true,
         message: 'Animales obtenidos correctamente',
-        data: animals
-      });
-    });
-  },
-
-  updateAnimal(req, res) {
-    const id = req.params.id;
-    const animal = req.body;
-    Animal.updateById(id, animal, (err, data) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          message: 'Error al actualizar el animal',
-          error: err
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        message: 'Animal actualizado correctamente',
-        data: data
-      });
-    });
-  },
-
-  deleteAnimal(req, res) {
-    const id = req.params.id;
-    Animal.deleteById(id, (err, data) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          message: 'Error al eliminar el animal',
-          error: err
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        message: 'Animal eliminado correctamente',
-        data: data
+        data: animals // Datos de todos los animales
       });
     });
   }
--- api/models/animal.js
+++ api/models/animal.js
@@ -26,7 +26,7 @@ Animal.create = (animal, result) => {
     ],
     (err, res) => {
       if (err) {
-        console.log('Error al crear el animal: ', err);
+        console.log('error: ', err);
         result(err, null);
       } else {
         console.log('Id del nuevo animal: ', res.insertId);
@@ -34,77 +34,6 @@ Animal.create = (animal, result) => {
       }
     }
   );
-};
-
-Animal.findById = (id, result) => {
-  const sql = `SELECT * FROM animales WHERE ID_Animal = ?`;
-  db.query(sql, [id], (err, animal) => {
-    if (err) {
-      console.log('Error al consultar el animal: ', err);
-      result(err, null);
-    } else {
-      console.log('Animal consultado: ', animal[0]);
-      result(null, animal[0]);
-    }
-  });
-};
-
-Animal.getAll = (result) => {
-  const sql = `SELECT * FROM animales`;
-  db.query(sql, (err, animals) => {
-    if (err) {
-      console.log('Error al obtener los animales: ', err);
-      result(err, null);
-    } else {
-      console.log('Animales obtenidos correctamente');
-      result(null, animals);
-    }
-  });
-};
-
-Animal.updateById = (id, animal, result) => {
-  const sql = `
-    UPDATE animales
-    SET Nombre = ?,
-        Raza = ?,
-        Sexo = ?,
-        Edad = ?,
-        ID_Estado = ?,
-        Imagen = ?
-    WHERE ID_Animal = ?
-  `;
-  const values = [
-    animal.nombre,
-    animal.raza,
-    animal.sexo,
-    animal.edad,
-    animal.ID_Estado,
-    animal.imagen,
-    id
-  ];
-
-  db.query(sql, values, (err, res) => {
-    if (err) {
-      console.log('Error al actualizar el animal: ', err);
-      result(err, null);
-    } else {
-      console.log('Animal actualizado correctamente');
-      result(null, res);
-    }
-  });
-};
-
-Animal.deleteById = (id, result) => {
-  const sql = `DELETE FROM animales WHERE ID_Animal = ?`;
-  db.query(sql, [id], (err, res) => {
-    if (err) {
-      console.log('Error al eliminar el animal: ', err);
-      result(err, null);
-    } else {
-      console.log('Animal eliminado correctamente');
-      result(null, res);
-    }
-  });
 };
 
 module.exports = Animal;
