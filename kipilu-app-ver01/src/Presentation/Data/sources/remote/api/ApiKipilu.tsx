9348cf39f3a385227f0b0bdbcb192554b8c23df7
Manuelyjc <114593318+Manuelyjc@users.noreply.github.com>
2024-06-10 14:00:40 -0500

    Update ApiKipilu.tsx

--- kipilu-app-ver01/src/Presentation/Data/sources/remote/api/ApiKipilu.tsx
+++ kipilu-app-ver01/src/Presentation/Data/sources/remote/api/ApiKipilu.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const ApiKipilu = axios.create({
 
-  baseURL: 'http://192.168.10.16:3000/api',
+  baseURL: 'https://kipilubackendrepository-3.onrender.com/api',
   headers: {
   'Content-Type': 'application/json'
  }
